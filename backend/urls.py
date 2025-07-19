4577fe341fd7f2a57afd9e4a684c01cdd1a23d1f
winniekathomi <kathomimemba@gmail.com>
2025-07-19 09:09:48 +0300

    changes to backend/urls.py

--- backend/urls.py
+++ backend/urls.py
@@ -14,17 +14,21 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from django.http import JsonResponse
+from django.urls import path, re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="My API",
+        default_version='v1',
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-def health_check(request):
-    return JsonResponse({"status": "ok"})
-
-urlpatterns += [
-    path('health/', health_check),
+    # ... your other URLs
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
